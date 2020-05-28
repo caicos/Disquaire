fd351d116018171feb12ec3b9dfa7845bda02465
caicos <bruletmarc@yahoo.fr>
2020-05-28 09:27:40 +0200

    Ajoutez un formulaire de réservation - avant implémentation du module Form dans les gabarits

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Album, Artist, Contact, Booking
+# from .forms import ContactForm
 
 def index(request):
     albums = Album.objects.filter(available = True).order_by('-created_at')[:12]
@@ -12,9 +13,12 @@ def index(request):
 
 def listing(request):
     albums_list = Album.objects.filter(available = True)
+    # Slice pages
     paginator = Paginator(albums_list, 9)
+    # Get current page numbe
     page = request.GET.get('page')
     try:
+        # Return only this page albums and not others
         albums = paginator.page(page)
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
@@ -32,11 +36,36 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk = album_id)
     artists = [artist.name for artist in album.artists.all()]
     artists_name = " ".join(artists)
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+
+        contact = Contact.objects.filter(email = email)
+        if not contact.exists():
+            # Si un contact n'est pas enregistré, on créé un nouveau contact.
+            contact = Contact.objects.create(
+                email = email,
+                name = name
+            )
+        album = get_object_or_404(Album, id = album_id)
+        booking = Booking.objects.create(
+            contact = contact,
+            album = album
+        )
+        album.available = False
+        album.save()
+        context = {
+            'album_title':album.title
+        }
+        return render(request, 'store/merci.html', context)
+    # else:
+    #     form = ContactForm()
     context = {
-        'album_title':album.title,
-        'artists_name':artists_name,
-        'album_id':album.id,
-        'thumbnail':album.picture
+        'album_title': album.title,
+        'artists_name': artists_name,
+        'album_id': album.id,
+        'thumbnail': album.picture,
+        # 'form': form
     }
     return render(request, 'store/detail.html', context)
 
